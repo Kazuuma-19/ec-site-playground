f4d1ae9ace1b001aac9dfe3a116cd212a7dc3355
Kazuuma-19 <kazu15870@gmail.com>
2025-05-28 16:05:19 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ECサイト(検証用)
+
+## Get Started
+### 1. 環境構築
+- DBを作成
+```bash
+CREATE DATABASE IF NOT EXISTS ec_site_playground;
+```
---
+++ backend/src/test/java/com/example/ec_site_playground/EcSitePlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec_site_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcSitePlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
