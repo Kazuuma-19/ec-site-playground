f27f8e81489d6124763e448efb7f30f0044ee72e
Kazuuma-19 <kazu15870@gmail.com>
2025-05-28 17:18:51 +0900

    feat: add formatter and linter

---
+++ backend/src/main/java/com/example/ec/site/playground/EcSitePlaygroundApplication.java
@@ -0,0 +1,18 @@
+package com.example.ec.site.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** Applicationクラス. */
+@SpringBootApplication
+public class EcSitePlaygroundApplication {
+
+  /**
+   * main method.
+   *
+   * @param args string
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(EcSitePlaygroundApplication.class, args);
+  }
+}
