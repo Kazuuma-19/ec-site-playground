1fc03d6edf302ba81cbff082cc440a855eeffe7e
Kazuma <kazu15870@gmail.com>
2025-06-14 12:18:14 +0900

    chore: topping用のinsert文を追加

---
+++ backend/document/sql/insert-toppings.sql
@@ -0,0 +1,5 @@
+INSERT INTO topping (topping_name, price_m, price_l) VALUES ('チーズ', 100, 150);
+INSERT INTO topping (topping_name, price_m, price_l) VALUES ('ベーコン', 120, 180);
+INSERT INTO topping (topping_name, price_m, price_l) VALUES ('マッシュルーム', 80, 130);
+INSERT INTO topping (topping_name, price_m, price_l) VALUES ('オニオン', 70, 100);
+INSERT INTO topping (topping_name, price_m, price_l) VALUES ('ピーマン', 60, 90);
